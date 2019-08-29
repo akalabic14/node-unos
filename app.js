380bf90223e8bb7157c30e7645d1efc03535577f
Micca Markovic <micco.markovic@gmail.com>
2019-08-29 21:58:09 +0200

    added form validator

--- app.js
+++ app.js
@@ -2,6 +2,9 @@
 const express = require('express')
 
 const bodyParser = require('body-parser')
+
+const { check, validationResult } = require('express-validator/check')
+
 const Entry = require('./index')
 
 const app = express()
@@ -21,16 +24,32 @@ app.get('/', (req, res) => {
     .catch(err => console.log('some error', err))
  })
 
-app.post('/', (req,res) => {
+app.post('/', [
+
+    check('id').isLength({min: 1}).withMessage('Empty ID'),
+    check('persons').isLength({min: 1}).withMessage('Empty Persons'),
+    check('code').isLength({min: 1}).withMessage('Empty Code')
+
+],(req,res) => {
+    
+    var errors = validationResult(req)
+
+    console.log(errors.array());
+
+    if (!errors.isEmpty()) {
+        
+        Entry.findAll({limit: 5, order: [['time', 'DESC']]})
+        .then(entries => res.render('index', {entries: entries, errors: errors.array()}))
+        
+       
+    } else {
+      
     let formData = req.body
     formData.time = new Date()
     Entry.create(formData)
-    res.redirect('back')
+    res.redirect('back')}
        
-
-
-    console.log(req.body.name)})
-
+})
 
 app.listen(3000, () => console.log('app is running'))
 
