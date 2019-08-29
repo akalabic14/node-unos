97031bbcad096c40d49c8e934f3cd698b6aa1db4
Micca Markovic <micco.markovic@gmail.com>
2019-08-29 14:08:54 +0200

    basic submit form

--- app.js
+++ app.js
@@ -1,38 +1,36 @@
 
 const express = require('express')
-const app = express()
+
 const bodyParser = require('body-parser')
 const Entry = require('./index')
+
+const app = express()
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
 
+
 app.set('view engine', 'pug')
+
 
 
 app.get('/', (req, res) => {
 
     Entry.findAll({limit: 5, order: [['time', 'DESC']]})
-    .then(entries => {
-        if(entries.length<5) {
-            res.render('index', {entries: entries})
-        }
-        else {
-            res.json(entries.slice(5))
-        }
-    })
+    .then(entries => res.render('index', {entries: entries}))
     .catch(err => console.log('some error', err))
-})
+ })
 
 app.post('/', (req,res) => {
     let formData = req.body
     formData.time = new Date()
     Entry.create(formData)
+    
     res.send('read')
     
 
 
-    console.log(formData.time)})
+    console.log(req.body.name)})
 
 
 app.listen(3000, () => console.log('app is running'))
